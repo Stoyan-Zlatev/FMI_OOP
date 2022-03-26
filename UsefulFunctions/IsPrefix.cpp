0baab73d9dfea595511d08f48898faf9bd2e8b38
Стоян Златев <47269030+Tony5768@users.noreply.github.com>
2022-03-26 16:48:02 +0200

    Create IsPrefix.cpp

---
+++ UsefulFunctions/IsPrefix.cpp
@@ -0,0 +1,11 @@
+bool isPrefix(const char* text, const char* prefix)
+{
+	int i = 0;
+	while (prefix[i] != '\0')
+	{
+		if (text[i] == '\0' || text[i] != prefix[i])
+			return false;
+		i++;
+	}
+	return true;
+}
