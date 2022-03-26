44bcc33f15b56cfd774e770f757e6e818e232e1f
Стоян Златев <stoyanzlatev22@gmail.com>
2022-03-26 15:09:44 +0200

    Update

--- Homework1/Exercise_1/Exercise_1.cpp
+++ Homework1/Exercise_1/Exercise_1.cpp
@@ -12,43 +12,9 @@ size_t getFileSize(std::fstream& file)
 	return result;
 }
 
-size_t getNumberLength(size_t number)
+void convertHexToDec()
 {
-	size_t counter = 0;
-	while (number != 0)
-	{
-		number /= 10;
-		counter++;
-	}
-	return counter;
-}
 
-void convertDecToHex(size_t number, char* hexNumber)
-{
-	size_t numberLength = getNumberLength(number);
-
-	if (numberLength == 0)
-	{
-		hexNumber[0] = 48;
-		hexNumber[1] = 48;
-	}
-	else if (number < 16)
-	{
-		hexNumber[0] = 0;
-		hexNumber[1] = number % 16;
-	}
-	else
-	{
-		for (size_t i = numberLength; i > 0; i--)
-		{
-			size_t lastDigit = number % 16;
-			if (lastDigit < 10)
-				hexNumber[i - 1] = lastDigit + 48;
-			else
-				hexNumber[i - 1] = lastDigit + 55;
-			number /= 16;
-		}
-	}
 }
 
 void convertArrayToHex(int* arr, size_t length, char* array)
@@ -60,15 +26,6 @@ void convertArrayToHex(int* arr, size_t length, char* array)
 		array[i * 2] = hexNumber[0];
 		array[i * 2 + 1] = hexNumber[1];
 	}
-}
-
-void printHex(char* hexInput, size_t fileSize)
-{
-	for (size_t i = 0; i < fileSize * 2; i += 2)
-	{
-		std::cout << hexInput[i] << hexInput[i + 1] << " ";
-	}
-	std::cout << std::endl;
 }
 
 int main()
@@ -101,12 +58,6 @@ int main()
 		input[i] = sourceFile.get();
 		std::cout << input[i] << std::endl;
 	}
-	//delete
-	char* hexInput = new char[fileSize * 2]; //new array with decimal to hexadecimal bytes
-
-	convertArrayToHex(input, fileSize, hexInput);
-	printHex(hexInput, fileSize);
-
 	for (size_t i = 0; i < fileSize; i++)
 	{
 		if ((input[i] <= 90 && input[i] >= 65) || (input[i] >= 97 && input[i] <= 122))
