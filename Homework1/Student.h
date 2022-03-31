95b5fe3293656a72ba95b530b2a5fef9d17c471e
Стоян Златев <47269030+Tony5768@users.noreply.github.com>
2022-03-31 23:18:35 +0300

    Create Student.h

---
+++ Homework1/Student.h
@@ -0,0 +1,29 @@
+#include "GenderType.h"
+#include "Exx 2.h"
+#include <cstring>
+
+#pragma warning(disable:4996)
+
+class Student
+{
+	char name[25];
+	int fn;//unique
+	size_t age;//[15,65]
+	GenderType gender;
+	char email[25];//contains(@)
+	double avg;//[2,6]
+public:
+	bool setName(const char* name);
+	bool setFn(size_t fn);
+	bool setAge(size_t age);
+	bool setGender(GenderType gender);
+	bool setEmail(const char* email);
+	bool setAvg(double avg);
+
+	const char* getName() const;
+	int getFn() const;
+	size_t getAge() const;
+	GenderType getGender() const;
+	const char* getEmail() const;
+	double getAvg() const;
+};
