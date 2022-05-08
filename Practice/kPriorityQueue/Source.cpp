d279194e55671549e40dcbd80a4f7baabd27e3e5
Стоян Златев <47269030+Tony5768@users.noreply.github.com>
2022-05-08 22:49:48 +0300

    Update Source.cpp

--- Practice/kPriorityQueue/Source.cpp
+++ Practice/kPriorityQueue/Source.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <exception>
+#include "kPriorityQueue.hpp"
 
+int main()
+{
+	try {
+		kPriorityQueue<char> q(4); // 4 priorities - 0, 1, 2 и 3.
+
+		q.push('A', 0); //adds A with lowest priority: 0
+		q.push('B', 3);
+		q.push('C', 2);
+		q.push('D', 2);
+		q.push('E', 1);
+		//q.push('F', 5); //error! No such priority!
+
+		q.peek(); // B
+		q.pop();
+
+		q.peek(); // C
+		q.pop();
+
+		q.peek(); // D
+		q.pop();
+
+		q.peek(); // E
+		q.pop();
+
+		q.peek(); // A
+		q.pop();
+	}
+	catch (std::logic_error& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
