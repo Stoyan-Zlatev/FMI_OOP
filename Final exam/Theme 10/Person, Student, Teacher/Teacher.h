6e003085c2392bf0c949c42d5fd4b206621fd25f
Стоян Златев <47269030+Tony5768@users.noreply.github.com>
2022-06-29 22:26:05 +0300

    Add files via upload

---
+++ Final exam/Theme 10/Person, Student, Teacher/Teacher.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Person.h"
+
+class Teacher : public Person
+{
+	char** subjects;
+	size_t subjectsCount;
+
+	void free();
+	void copyFrom(const Teacher& other);// само нещата, които са от Teacher
+public:
+	Teacher(const char* name, int age, const char* const* subjects, size_t subjectsCount);
+
+	Teacher(const Teacher& other);
+	Teacher& operator=(const Teacher& other);
+	~Teacher();
+};
