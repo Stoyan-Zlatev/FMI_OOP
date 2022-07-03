f3d5b058efb5bd327cadbc24229c31b0b28c51ea
Стоян Златев <47269030+Tony5768@users.noreply.github.com>
2022-07-04 01:32:13 +0300

    Update FileRowsCount.cpp

--- Final exam/Theme 02/FileRowsCount.cpp
+++ Final exam/Theme 02/FileRowsCount.cpp
@@ -1,35 +1,23 @@
 #include <iostream>
 #include <fstream>
 
-const size_t BUFF = 1024;
+const size_t BUFF_SIZE = 1024;
 
 size_t getFileRowsCount(std::ifstream& file)
 {
 	size_t currentPosition = file.tellg();
 	file.seekg(0, std::ios::beg);
+
+	size_t rows = 0;
+	char buff[BUFF_SIZE];
 	
-	size_t counter = 0;
-	char buff[BUFF];
 	while (!file.eof())
 	{
-		file.getline(buff,BUFF);
-		counter++;
+		file.getline(buff, BUFF_SIZE);
+		rows++;
 	}
 
-	file.seekg(0, currentPosition);
-	return counter;
-}
-int main()
-{
-	std::ifstream file("name.txt");
-
-	if (!file.is_open())
-	{
-		std::cout << "Error while opening the file!" << std::endl;
-		return -1;
-	}
-
-	std::cout << getFileRowsCount(file) << " rows" << std::endl;
-
-	return 0;
+	file.seekg(currentPosition);
+	
+	return rows;
 }
