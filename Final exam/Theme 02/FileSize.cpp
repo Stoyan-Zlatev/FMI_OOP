a2118c02c33fc4209c8413211fe5aa1b780b941a
Стоян Златев <47269030+Tony5768@users.noreply.github.com>
2022-07-04 01:30:20 +0300

    Update FileSize.cpp

--- Final exam/Theme 02/FileSize.cpp
+++ Final exam/Theme 02/FileSize.cpp
@@ -5,7 +5,7 @@ size_t getFileSize(std::ifstream& file)
 {
 	size_t currentPosition = file.tellg();
 
-	file.seekg(0, std::ios::cur);
+	file.seekg(0, std::ios::end);
 	size_t size = file.tellg();
 
 	file.seekg(currentPosition);
