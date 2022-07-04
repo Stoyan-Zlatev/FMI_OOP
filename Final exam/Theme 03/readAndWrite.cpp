0721f60d698a0da97849d71b0a1d9c12258b786b
Стоян Златев <47269030+Tony5768@users.noreply.github.com>
2022-07-04 14:31:40 +0300

    Update readAndWrite.cpp

--- Final exam/Theme 03/readAndWrite.cpp
+++ Final exam/Theme 03/readAndWrite.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <fstream>
 #include <cstring>
-
-#pragma warning(disable:4996)
+#include <fstream>
 
 const size_t MAX_NAME_SIZE = 22;
 struct Student
@@ -12,7 +10,7 @@ struct Student
 	size_t fn;
 };
 
-void initStudent(Student& st, const char* name, size_t fn, bool isMale)
+void initStudent(Student& st, const char* name, bool isMale, size_t fn)
 {
 	if (name == nullptr || strlen(name) > MAX_NAME_SIZE)
 		return;
@@ -22,72 +20,67 @@ void initStudent(Student& st, const char* name, size_t fn, bool isMale)
 	st.fn = fn;
 }
 
-void saveToFile(const Student* students, size_t count, std::ofstream& file)
+void saveToFile(std::ofstream& file, const Student* students, size_t studentsCount)
 {
-	file.write((const char*)students, count * sizeof(Student));
+	file.write((const char*)students, studentsCount * sizeof(Student));
 }
 
-size_t getFileSize(std::ifstream& f)
+size_t getFileSize(std::ifstream& file)
 {
-	size_t currentPos = f.tellg();
-	f.seekg(0, std::ios::end);
-	size_t size = f.tellg();
+	size_t currPos = file.tellg();
+	file.seekg(0, std::ios::end);
 
-	f.seekg(currentPos);
+	size_t size = file.tellg();
+	file.seekg(currPos);
+
 	return size;
 }
 
-void readFromFile(Student*& ptr, size_t& studentsCount, std::ifstream& f)
+void readFromFile(std::ifstream& file, const Student* students)
 {
-	size_t sizeOfFile = getFileSize(f);
-	studentsCount = sizeOfFile / sizeof(Student);
-	ptr = new Student[studentsCount];
-	f.read((char*)ptr, sizeOfFile);
+	size_t studentsCount = getFileSize(file) / sizeof(Student);
+	students = new Student[studentsCount];
+	file.read((char*)students, studentsCount * sizeof(Student));
 }
 
 int main()
 {
 	{
-		const size_t studentsCount = 4;
-		Student* arr = new Student[studentsCount];
-		initStudent(arr[0], "Ivan", 1, true);
-		initStudent(arr[1], "Petur", 2, true);
-		initStudent(arr[2], "Alex", 3, true);
-		initStudent(arr[3], "Katerina", 4, false);
+		const size_t studentsCount = 3;
+		Student* students = new Student[studentsCount];
+		initStudent(students[0], "Ivan", true, 2);
+		initStudent(students[1], "Peter", true, 3);
+		initStudent(students[2], "Ivana", false, 4);
 
 		std::ofstream file("students.dat", std::ios::binary);
 
 		if (!file.is_open())
 		{
-			std::cout << "Error while opening the file!" << std::endl;
-			delete[] arr; //!
+			std::cout << "Error, opening the file!";
+			delete[] students;
 			return -1;
 		}
 
-		saveToFile(arr, studentsCount, file);
+		saveToFile(file, students, studentsCount);
 
-		delete[] arr;
-
+		delete[] students;
 		return 0;
 	}
 	{
-		Student* arr;
-		size_t count;
+		Student* students;
+
 		std::ifstream file("students.dat", std::ios::binary);
 
 		if (!file.is_open())
 		{
-			std::cout << "Error while opening the file!" << std::endl;
+			std::cout << "Error, opening the file!";
+			delete[] students;
 			return -1;
 		}
 
-		readFromFile(arr, count, file);
+		readFromFile(file, students);
 
-		for (int i = 0; i < count; i++)
-			std::cout << "Name: " << arr[i].name << ", fn: " << arr[i].fn << ", is male: " << arr[i].isMale << std::endl;
-
-		delete[] arr;
-
+		delete[] students;
 		return 0;
 	}
 }
