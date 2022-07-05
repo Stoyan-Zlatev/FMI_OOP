c3bda2935f4260e2ee9a5b2ccfbcbcf95c568e38
Стоян Златев <47269030+Tony5768@users.noreply.github.com>
2022-07-05 18:48:52 +0300

    Update Complex.h

--- Final exam/Theme 07/Comples/Complex.h
+++ Final exam/Theme 07/Comples/Complex.h
@@ -9,23 +9,17 @@ public:
 	Complex();
 	Complex(double real, double im);
 
-	Complex getConjugated() const;  //връща комплексно спрегнатото число на текущия обект.
+	Complex getConjugated() const;
 
-	//assignment operators 
-	// a+=b Към а се добавя стойността на b. а се променя , b не се. Операцията  ще върне a за да може да се правят верижни изрази (a+=b+=c)
 	Complex& operator+=(const Complex&);
 	Complex& operator-=(const Complex&);
 	Complex& operator*=(const Complex&);
 	Complex& operator/=(const Complex&);
 
-
-
-	//stream opeartors   ( std::cout << c1)  (std::cin >> c1 )
 	friend std::ostream& operator<<(std::ostream&, const Complex&);
 	friend std::istream& operator>>(std::istream&, Complex&);
 
 };
-// a+b. Операторът връща НОВА променлива(Complex), която е резултатът от операцията.
 Complex operator+(const Complex&, const Complex&);
 Complex operator-(const Complex&, const Complex&);
 Complex operator*(const Complex&, const Complex&);
