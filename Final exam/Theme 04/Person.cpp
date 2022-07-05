0d0d2cdee9cbe969b3a1b09e384dae49ee278d84
Стоян Златев <47269030+Tony5768@users.noreply.github.com>
2022-07-05 14:25:51 +0300

    Update Person.cpp

--- Final exam/Theme 04/Person.cpp
+++ Final exam/Theme 04/Person.cpp
@@ -1,44 +1,31 @@
 #include <iostream>
+#include <cstring>
 
-const size_t MinAge = 5;
-const size_t MaxAge = 90;
-const size_t MaxNameLength = 20;
+#pragma warning(disable:4996)
 
-class Student
+const size_t MAX_NAME_SIZE = 20;
+const size_t MAX_AGE = 90;
+const size_t MIN_AGE = 5;
+
+class Person
 {
-	char name[MaxNameLength + 1];
+	char name[MAX_NAME_SIZE + 1];
 	size_t age;
-
-	bool isValidAge(size_t age)
-	{
-		return age >= MinAge && age <= MaxAge;
-	}
 
 	bool isValidName(const char* name)
 	{
-		if (name == nullptr)
-			return false;
+		return (name != nullptr) && strlen(name) <= MAX_NAME_SIZE;
+	}
 
-		if (strlen(name) >= MaxNameLength)
-			return false;
-
-		return true;
+	bool isValidAge(size_t age)
+	{
+		return age <= MAX_AGE && age >= MIN_AGE;
 	}
 public:
-	Student(const char* name, size_t age)
+	Person(const char* name, size_t age)
 	{
 		setName(name);
 		setAge(age);
-	}
-
-	size_t getAge() const
-	{
-		return age;
-	}
-
-	const char* getName() const
-	{
-		return name;
 	}
 
 	void setName(const char* name)
@@ -47,18 +34,32 @@ public:
 			strcpy(this->name, name);
 		else
 			strcpy(this->name, "Unknown");
-
 	}
-	void setAge(int age)
+
+	void setAge(size_t age)
 	{
 		if (isValidAge(age))
 			this->age = age;
 		else
-			this->age = MinAge;
+			this->age = MIN_AGE;
+
+	}
+
+	const char* getName() const
+	{
+		return name;
+	}
+
+	size_t getAge() const
+	{
+		return age;
 	}
 };
 
 int main()
 {
-	Student s("Ivan", 33);
+	Person p1("Ivan", 13);//OK
+	Person p2("Ivan", 95);//Error 95>90
+	Person p3("Anastasius Papastopolus", 50);//Unknown
+	std::cout << p3.getName();
 }
